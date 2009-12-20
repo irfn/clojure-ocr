c4641074e5d5b836e89fb78d1dbed84a99ee9c69
irfn <mail.urfi@gmail.com>
2009-12-20 21:41:57 +0530

    refactored string util code to string-ops

---
+++ src/string_ops.clj
@@ -0,0 +1,22 @@
+(ns string-ops)		
+(require '[clojure.contrib.str-utils2 :as s])
+
+(defn only-a-tip?
+	"True if s is of length 0 or 1"
+	[line-of-text]
+	(>= 1 (.length line-of-text)))
+
+(defn lines
+	"splits by newlines"
+	[text]
+	(seq (.split (first text) "\n")))
+
+(defn tip
+	"tip of a string"
+	[string-value]
+	(str (first string-value)))
+
+(defn tail
+	"tip of a string"
+	[string-value]
+	(apply str (rest string-value)))
---
+++ test/test/string_ops.clj
@@ -0,0 +1,15 @@
+(ns test.string-ops
+  (:use clojure.test)
+  (:use string-ops))
+
+(deftest test-lines
+	(are [description split-lines some-text] (= split-lines (lines some-text))
+  "splitting lines" ["  _  _     _  _  _  _  _",
+										 "| _| _||_||_ |_   ||_||_|",
+										 "||_  _|  | _||_|  ||_| _|"] ["  _  _     _  _  _  _  _\n| _| _||_||_ |_   ||_||_|\n||_  _|  | _||_|  ||_| _|"]))
+
+(deftest test-tip
+  (is (= "t" (tip "tip"))))
+
+(deftest test-tail
+  (is (= "ail" (tail "tail"))))
