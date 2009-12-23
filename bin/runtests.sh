c89da89031379a9c0367c68884caf4fcb40bb2da
irfn <mail.urfi@gmail.com>
2009-12-24 00:30:05 +0530

    fix warning

--- bin/runtests.sh
+++ bin/runtests.sh
@@ -1,1 +1,1 @@
-java -Xmx1G -cp test:src:lib/clojure.jar:lib/clojure-contrib.jar:lib/hsqldb.jar:lib/ant.jar:lib/ant-launcher.jar clojure.lang.Script test/test.clj
+java -Xmx1G -cp test:src:lib/clojure.jar:lib/clojure-contrib.jar clojure.main -i test/test.clj
